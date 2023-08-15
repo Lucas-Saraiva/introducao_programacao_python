64da29f211d821d749a386b088e58ef77f753662
Lucas-saraiva <lukirita22@gmail.com>
2023-08-15 06:54:39 -0300

    Operadores Relacionais

---
+++ OperadoresRelacionais/prog-000.py
@@ -0,0 +1,12 @@
+a = 1
+b = 5
+c = 2
+d = 1
+print(a == b)
+print(b > a)
+print(a < b)
+print(a == d)
+print(b >= a)
+print(c <= b)
+print(d != a)
+print(d != b)
---
+++ OperadoresRelacionais/prog-002.py
@@ -0,0 +1,4 @@
+nota = 8
+média = 7
+aprovado = 8 > 7
+print(aprovado)
